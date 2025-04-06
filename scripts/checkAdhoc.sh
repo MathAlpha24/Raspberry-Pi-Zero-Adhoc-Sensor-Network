2d155857d1919f67992a6b37bf1c45acf476fd8a
Ethan McKendell <145165919+emmckendell@users.noreply.github.com>
2025-04-06 01:15:51 -0700

    Update checkAdhoc.sh

--- scripts/checkAdhoc.sh
+++ scripts/checkAdhoc.sh
@@ -3,6 +3,12 @@
 # Define the network range for the Ad-Hoc network
 network="192.168.2"
 
+# Get the current IP address of the system
+current_ip=$(hostname -I | awk '{print $1}')
+
+# Display the current IP address
+echo "Current IP address: $current_ip"
+
 # Use arp command to list devices connected to the network
 # Filter out devices on the same subnet (you can modify the network part as needed)
 connected_devices=$(arp -n | grep "$network" | awk '{print $1}')
